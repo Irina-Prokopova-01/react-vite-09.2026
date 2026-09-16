cb2d6b62c018d98ad293dc651159bda71718cc01
Irina <Irina-prokopova.style@yandex.ru>
2026-09-15 22:51:46 -0700

    Созданы запросы к серверу на get, putch delete

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,17 +1,7 @@
 import {useCallback, useEffect, useMemo, useRef, useState} from "react";
-import useTasksLocalStorage from "./useTasksLocalStorage.js";
 
 const useTasks = () => {
-    const {
-        savedTasks,
-        saveTasks,
-    } = useTasksLocalStorage(
-
-    )
-    const [tasks, setTasks] = useState(savedTasks ?? [
-        {id: 'task-1', title: 'Купить молоко', isDone: false},
-        {id: 'task-2', title: 'Купить хлеб', isDone: true},
-    ])
+    const [tasks, setTasks] = useState([])
 
     const [newTaskTitle, setNewTaskTitle] = useState('')
     const [searchQuery, setSearchQuery] = useState('')
@@ -24,50 +14,76 @@ const useTasks = () => {
         const isConfirmed = confirm('Are you sure you want to delete all task?')
 
         if (isConfirmed) {
-            setTasks([])
+            Promise.all(
+                tasks.map(({id}) => {
+                    return fetch(`http://localhost:3001/tasks/${id}`, {
+                        method: "DELETE",
+                    })
+                })
+            ).then(() => setTasks([]))
         }
-    }, [])
+    }, [tasks])
 
     const deleteTask = useCallback(
         (taskId) => {
             // console.log(`Удаляем задачу с id: ${taskId}!`);
-            setTasks(
-                tasks.filter((task) => task.id !== taskId)
-            )
+
+            fetch(`http://localhost:3001/tasks/${taskId}`, {
+                method: 'DELETE',
+            })
+                .then(() => {
+                    setTasks(
+                        tasks.filter((task) => task.id !== taskId)
+                    )
+                })
+
         }, [tasks])
 
     const toggleTaskComplete = useCallback(
         (taskId, isDone) => {
             // console.log(`Задача ${taskId} ${isDone ? 'выполнена' : 'не выполнена'}`);
-            setTasks(
-                tasks.map((task) => {
-                    if (task.id === taskId) {
-                        return {...task, isDone}
-                    }
-                    return task
+            fetch(`http://localhost:3001/tasks/${taskId}`, {
+                method: "PATCH",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({isDone})
+            })
+                .then(() => {
+                    setTasks(
+                        tasks.map((task) => {
+                            if (task.id === taskId) {
+                                return {...task, isDone}
+                            }
+                            return task
+                        }))
                 })
-            )
 
         }, [tasks])
 
     const addTask = useCallback((title) => {
         // const newTaskTitle = newTaskInputRef.current.value
             const newTask = {
-                id: crypto?.randomUUID() ?? Date.now().toString(),
                 title,
                 isDone: false,
             }
 
-            setTasks((prevTasks) => [...prevTasks, newTask]);
-            setNewTaskTitle('')
-            setSearchQuery('')
-            newTaskInputRef.current.focus()
+            fetch(`http://localhost:3001/tasks`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newTask),
+            })
+                .then(response => response.json())
+                .then(addedTask => {
+                    setTasks((prevTasks) => [...prevTasks, addedTask]);
+                    setNewTaskTitle('')
+                    setSearchQuery('')
+                    newTaskInputRef.current.focus()
+                })
 
     }, [])
-
-    useEffect(() => {
-        saveTasks(tasks)
-    }, [tasks])
 
     const filteredTasks = useMemo(() => {
         const clearSearchQuery = searchQuery.trim().toLowerCase()
@@ -78,6 +94,10 @@ const useTasks = () => {
 
     useEffect(() => {
         newTaskInputRef.current.focus()
+
+        fetch('http://localhost:3001/tasks')
+        .then(response => response.json())
+        .then(setTasks)
 
     }, [])
 
