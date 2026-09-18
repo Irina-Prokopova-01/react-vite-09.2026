4fb74a9de79e546c48ca0009d9be92a84814d008
Irina <Irina-prokopova.style@yandex.ru>
2026-09-18 15:51:22 -0700

    Теперь динамический роутер работает не только для task но и для любых других.

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,29 @@
 import {useEffect, useState} from "react";
 
+const matchPath = (path, route) => {
+    const pathPaths = path.split("/");
+    const routePaths = route.split("/");
+
+    if (pathPaths.length !== routePaths.length) {
+        return null
+    }
+
+    const params = {}
+
+    for (let i = 0; i < routePaths.length; i++) {
+        if (routePaths[i].startsWith(':')) {
+            const paramName = routePaths[i].slice(1)
+
+            params[paramName] = pathPaths[i]
+        } else if (
+            routePaths[i] !== pathPaths[i]
+        ) {
+            return null
+        }
+    }
+    return params
+}
+
 export  const useRouter = () => {
     const [path, setPath] = useState(window.location.pathname)
 
@@ -24,14 +48,27 @@ const Router = (props) => {
     const { routes } = props
     const path = useRouter()
 
-    if (path.startsWith('/tasks')) {
-        const id = path.replace('/tasks/', '')
-        const TaskPage = routes['/tasks/:id']
+    for (const route in routes) {
+        const params = matchPath(path, route)
 
-        return <TaskPage params={{ id }} />
+        if (params) {
+            const Page = routes[route]
+
+            return <Page params={params} />
+        }
     }
-    const Page = routes[path] ?? routes['*']
-    return <Page />
+    const NotFound = routes['*']
+
+    return <NotFound />
+
+    // if (path.startsWith('/tasks')) {
+    //     const id = path.replace('/tasks/', '')
+    //     const TaskPage = routes['/tasks/:id']
+    //
+    //     return <TaskPage params={{ id }} />
+    // }
+    // const Page = routes[path] ?? routes['*']
+    // return <Page />
 }
 
 export default Router
